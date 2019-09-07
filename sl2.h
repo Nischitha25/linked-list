f0b0773068f437bcdf22023f71d55e57ee7666c7
NischithaAC <55012946+NischithaAC@users.noreply.github.com>
2019-09-07 09:53:17 -0700

    Update sl2.h

--- sl2.h
+++ sl2.h
@@ -28,15 +28,15 @@ uint32_t slist_length(Slist *list);
 uint32_t slist_lookup(Slist *list,int32_t key);
 Slist* slist_add_head(Slist *list,int32_t element);
 Slist* slist_add_tail(Slist *list,int32_t element);
-//Slist* slist_delete_head(Slist *list);
-//Slist* slist_delete_tail(Slist *list);
+Slist* slist_delete_head(Slist *list);
+Slist* slist_delete_tail(Slist *list);
 Slist* slist_display(Slist *list);
-//Slist* slist_min_max(Slist *list);
-//Slist* slist_spec_ele(Slist *list, int32_t element,int32_t spec_ele);
-//Slist* slist_spec_ele_delete(Slist *list, int32_t spec_ele);
-//uint32_t list_compare(Slist *list1,Slist *list2);
+Slist* slist_min_max(Slist *list);
+Slist* slist_spec_ele(Slist *list, int32_t element,int32_t spec_ele);
+Slist* slist_spec_ele_delete(Slist *list, int32_t spec_ele);
+uint32_t list_compare(Slist *list1,Slist *list2);
 Slist* reverse_list(Slist *list);
-//Slist* union_twolist(Slist *union_list,Slist *list1,Slist *list2);
+Slist* union_twolist(Slist *union_list,Slist *list1,Slist *list2);
 Slist* intersection_twolist(Slist *intersection_list,Slist *list1,Slist *list2);
-//Slist* unique_slist(Slist* list,uint32_t element);
-#endif // 
+Slist* unique_slist(Slist* list,uint32_t element);
+#endif  
